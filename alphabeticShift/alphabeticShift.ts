6e4b70f93d4444e15ab48beba8b24d0921e01987
keza <dk@citadel.rs>
2018-07-06 18:18:25 +0200

    Add comment

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -2,7 +2,7 @@ function alphabeticShift(inputString: string): string {
     let result: string = "";
     for (let i = 0; i < inputString.length; i++) {
         let char: string = "", code: number = 0;
-        code = inputString[i].charCodeAt(0)+1
+        code = inputString[i].charCodeAt(0)+1 // Shift ascii code plus one
         if (code > 122) { // when letter was z, then next letter is a
             code = 97;
         }
