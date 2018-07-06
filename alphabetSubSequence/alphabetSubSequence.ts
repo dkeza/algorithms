9f2f58ca416379fd2241a77427d65ebdc28e9b5c
keza <dk@citadel.rs>
2018-07-06 18:54:58 +0200

    Solution

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,7 +1,26 @@
 function alphabetSubsequence(s: string): boolean {
-
+    let result: boolean = true, prevChar: string = "";
+    const charsChecked = new Set();
+    for (let e of s) {
+        // Check if current char is before previous char (compared on ascii code table)
+        if (prevChar.length>0 && e.charCodeAt(0) < prevChar.charCodeAt(0)) {
+            result = false;
+            break;
+        }
+        // Check if current was char already found in string
+        if (charsChecked.has(e)) {
+            result = false;
+            break;
+        }
+        // Add current char to set, to check it in next iteration
+        charsChecked.add(e);
+        // Store current char for check in next iteration
+        prevChar = e;
+    }
+    return result;
 }
 
+console.log(alphabetSubsequence('abc'))
 console.log(alphabetSubsequence('zab'))
 console.log(alphabetSubsequence('effg'))
 console.log(alphabetSubsequence('cdce'))
