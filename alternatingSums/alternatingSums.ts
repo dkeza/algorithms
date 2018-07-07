212a546134504c55f61a12c0a3aa8cfcbefa57bf
keza <dk@citadel.rs>
2018-07-07 12:06:48 +0200

    Solution

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,13 @@
 function alternatingSums(a: number[]): number[] {
-
+    let result: number[] = [0,0];
+    for (let i = 1; i <= a.length; i++) {
+        if (i%2==0) {
+            result[1] += a[i-1];
+        } else {
+            result[0] += a[i-1];
+        }
+    }
+    return result;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
