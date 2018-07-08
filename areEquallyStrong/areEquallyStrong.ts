bba6cf68d9a2cf4496c386b3423244378a73ed98
keza <dk@citadel.rs>
2018-07-08 09:50:56 +0200

    Solution

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,15 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+    let result: boolean = false, yourStrongestArm: number = 0, yourWeakestArm: number = 0, friendsStrongestArm: number = 0, friendsWeakestArm: number = 0;
+    yourStrongestArm = Math.max(yourLeft, yourRight);
+    yourWeakestArm = Math.min(yourLeft, yourRight);
+    friendsStrongestArm = Math.max(friendsLeft, friendsRight);
+    friendsWeakestArm = Math.min(friendsLeft, friendsRight);
+    if (yourStrongestArm === friendsStrongestArm && yourWeakestArm === friendsWeakestArm) {
+        result = true;
+    }
+    return result;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
